b7191e4cadc9c6cb706fa1852e25ce73f4c9d859
multicampus <callu_9ine@naver.com>
2021-09-10 12:59:35 +0900

    S05P21B202-79 [feat] req.2-2 human_detector.py 구현

--- ros2_smart_home/sub2/sub2/human_detector.py
+++ ros2_smart_home/sub2/sub2/human_detector.py
@@ -51,8 +51,8 @@ def non_maximum_supression(bboxes, threshold=0.5):
             y1_br = bbox[1] + bbox[3]
             y2_br = new_bbox[1] + new_bbox[3]
             
-            """
             # 로직 4 : 두 bbox의 겹치는 영역을 구해서, 영역이 안 겹칠때 new_bbox로 save
+            """
             x_overlap = 
             y_overlap = 
             overlap_area = x_overlap * y_overlap
@@ -68,7 +68,27 @@ def non_maximum_supression(bboxes, threshold=0.5):
                 new_bboxes.append(bbox)
 
             """
-
+            x_len = (x1_br if x1_br > x2_br else x2_br) - (x1_tl if x1_tl < x2_tl else x2_tl)
+            x_overlap = (bbox[2] + new_bbox[2]) - x_len
+            x_overlap = x_overlap if x_overlap > 0 else 0
+            y_len = (y1_br if y1_br > y2_br else y2_br) - (y1_tl if y1_tl < y2_tl else y2_tl)
+            y_overlap = (bbox[3] + new_bbox[3]) - y_len
+            y_overlap = y_overlap if y_overlap > 0 else 0
+            overlap_area = x_overlap * y_overlap
+            
+            area_1 = bbox[2] * bbox[3]
+            area_2 = new_bbox[2] * new_bbox[3]
+            
+            total_area = area_1 + area_2 - overlap_area
+            overlap_area = overlap_area / float(total_area)
+            # print(f"total_area {total_area} , overlap_area {overlap_area}")
+            if overlap_area > threshold:
+                isNew = False
+                break
+        
+        if isNew :
+            new_bboxes.append(bbox)
+        
     return new_bboxes
 
 
@@ -120,35 +140,39 @@ class HumanDetector(Node):
             xl, yl, wl, hl = [], [], [], []
             rects = non_maximum_supression(rects_temp)
 
-            """
     
             # 로직 5 : bbox를 ros msg 파일에 write
 
             ## 각 bbox의 center, width, height의 꼭지점들을 리스트에 넣어 놓고
             ## 메세지 내 x,y,w,h에 채워넣는 방식으로 하시면 됩니다.
-           
             for (x,y,w,h) in rects:
     
-                xl.append(x)
-                yl.append(y)
-                wl.append(w)
-                hl.append(h)
+                xl.append(int(x))
+                yl.append(int(y))
+                wl.append(int(w))
+                hl.append(int(h))
 
             if self.able_to_pub:
 
-                self.bbox_msg.num_bbox = 
+                self.bbox_msg.num_bbox = len(rects)
+                # if len(rects) < 2 and len(rects_temp) == 2 :
+                #     print("low")
+                # if len(rects) > 2 :
+                #     print("high")
 
-                obj_list = 
+                obj_list = rects
 
-                self.bbox_msg.idx_bbox = 
+                idxl = []
+                for i in range(len(obj_list)) :
+                    idxl.append(i)
+                self.bbox_msg.idx_bbox = idxl
 
-                self.bbox_msg.x = 
-                self.bbox_msg.y = 
-                self.bbox_msg.w = 
-                self.bbox_msg.h = 
+                self.bbox_msg.x = xl
+                self.bbox_msg.y = yl
+                self.bbox_msg.w = wl
+                self.bbox_msg.h = hl
 
-            """
-
+            # 로직 6 : bbox를 원본 이미지에 draw
             for (x,y,w,h) in rects:
 
                 cv2.rectangle(img_bgr, (x,y),(x+w,y+h),(0,255,255), 2)
@@ -158,13 +182,14 @@ class HumanDetector(Node):
             self.bbox_msg.num_bbox = len(rects_temp)
 
 
-        """
-        로직 7 : bbox 결과 show
-        cv2.
+        # 로직 7 : bbox 결과 show
+        cv2.imshow("bbox_img", self.img_bgr)
         cv2.waitKey(1)
-        """           
+
+        """
         cv2.imshow("detection result", img_bgr)        
         cv2.waitKey(1)
+        """           
 
     def timer_callback(self):
 
