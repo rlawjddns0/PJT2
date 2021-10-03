07de9ffbcefc8b7a9587ba14d535d4007a00d906
rlawjddns0 <rlawjddns0@naver.com>
2021-10-03 23:25:34 +0900

    sdf

--- Backend/server/router/mode.js
+++ Backend/server/router/mode.js
@@ -105,9 +105,9 @@ router.get('/current_mode/:user_no',function(req,res){
 })
 
 router.get('/test/:index',function(req,res){
-    const index=req.params.index
-    const sql="select * from appliances where index=?"
-    DB.query(sql,[index],(err,data)=>{
+    const idx=req.params.index
+    const sql="select * from appliances where idx=?"
+    DB.query(sql,[idx],(err,data)=>{
         console.log(data);
         if(err){
             console.log(err)
